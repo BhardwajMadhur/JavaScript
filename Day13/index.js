aac39348c099afd101fa76dec4b56f62adf1f4ef
BhardwajMadhur <bmadhur4587@gmail.com>
2022-09-01 09:54:42 +0530

    commit

---
+++ Day13/index.js
@@ -0,0 +1,5 @@
+import { messege } from "./library.js";
+console.log(messege);
+
+import hiiiii from "./library.js"
+hiiiii("hello");
---
+++ Day13/library.js
@@ -0,0 +1,5 @@
+export let messege="hii";
+
+export default function(hiiii) {
+    console.log("hiihi");
+}
